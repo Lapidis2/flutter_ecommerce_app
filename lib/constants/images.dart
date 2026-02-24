303147bc6e6994ed9d6436fd25b0a68713d49932
Lapidis2 <hitlapidis2@gmail.com>
2026-02-24 15:48:25 +0200

    added categories

---
+++ lib/constants/images.dart
@@ -0,0 +1,8 @@
+class ImageConstants {
+  static const List<String> categories = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx90JTE1xBRpsy1K9hpjNUX2ybRRUtv3_sAQ&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz61-AEfF5n3jAtK4t4VjeSrz8Pi4J3gHomA&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTA6rqPT7iO5ImXvhfbdBcBKZ4meYsZ1S-lJg&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz61-AEfF5n3jAtK4t4VjeSrz8Pi4J3gHomA&s",
+  ];
+}
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,19 +1,85 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_ecommerce_app/constants/images.dart';
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold
-    (
-body: Center(
-        child: Text(
-          'Home Screen',
-          style: TextStyle(fontSize: 24),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'E-commerce',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'search anything',
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                'Categories',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+            children: ImageConstants.categories
+        .map(
+          (e) => Padding(
+            padding: const EdgeInsets.only(left: 12.0),
+            child: Card(
+              color: Colors.white,
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: SizedBox(
+                height: 100,
+                width: 100,
+                child: CachedNetworkImage(
+                  imageUrl: e,
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        )
+        .toList(),
+              ),
+            ),
+             Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                'Top Products',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
